3314c58564a53d3d1b81e07b845991e2cdfa948c
Erwan Thomy <thomy.erwan@gmail.com>
2025-03-21 15:47:56 +0100

    fixed some unoptimized code

--- src/Equipe.kt
+++ src/Equipe.kt
@@ -15,24 +15,35 @@ class Equipe {
         this.nbNuls = nbNuls
     }
 
-    fun ajouterJoueur(joueurs: MutableList<Joueur>): MutableList<Joueur>{
+    fun ajouterJoueur(): MutableList<Joueur>{
         println("Veuillez entrer les informations du joueur\n Nom, Position, NbButs, nbPasses, cartonsJaunes, cartonsRouges")
-        joueurs.add(Joueur(readln(), readln(), readln().toInt(), readln().toInt(), readln().toInt(), readln().toInt()))
-        return joueurs
+        this.joueurs.add(Joueur(readln(), readln(), readln().toInt(), readln().toInt(), readln().toInt(), readln().toInt()))
+        println("Le joueur a ete ajoute avec succes")
+        return this.joueurs
     }
 
-    fun retirerJoueur(joueurs: MutableList<Joueur>): MutableList<Joueur> {
-        for (joueur in joueurs) {
+    fun retirerJoueur(): MutableList<Joueur> {
+        for (joueur in this.joueurs) {
             println("${joueur.nom} => ${joueur.poste}")
         }
         println("Selectionner le joueur a retirer: ")
         joueurs.removeAll(joueurs.filter { it.nom == readln() })
+        println("Le joueur a ete retire avec succes")
 
         return joueurs
     }
 
-    fun afficherComposition(nom : String): MutableList<Joueur> {
-        return joueurs
+    fun afficherComposition(): MutableList<String> {
+        var listeJoueurs: MutableList<String> = mutableListOf()
+        for (joueur in this.joueurs) {
+            listeJoueurs.add(joueur.nom)
+        }
+        if (listeJoueurs.isEmpty()) {
+            println("Aucun joueur")
+            return mutableListOf()
+        }
+            println("Liste des joueurs de l'equipe " + this.nom + "\n" + listeJoueurs.toString())
+            return listeJoueurs;
     }
 
     fun calculerScoreEquipe(): Int {
--- src/index.kt
+++ src/index.kt
@@ -1,7 +1,8 @@
 fun main(){
     val FCbarcelona = Equipe("FC Barcelona", mutableListOf(), 0, 0, 0)
-    FCbarcelona.ajouterJoueur(FCbarcelona.joueurs)
-    FCbarcelona.retirerJoueur(FCbarcelona.joueurs)
+    FCbarcelona.ajouterJoueur()
+    FCbarcelona.retirerJoueur()
+    FCbarcelona.afficherComposition()
     println(FCbarcelona.calculerScoreEquipe())
 
 
